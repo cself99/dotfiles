b9ababf62c3284976c225036da78fa2843e98550
cself99 <cself99@gmail.com>
2020-04-08 11:08:03 -0400

    fix xresources colors, completely switch to alacritty, mpd finished

--- .startup.sh
+++ .startup.sh
@@ -2,7 +2,6 @@
 
 killall -q redshift
 unclutter &
-[ ! -s ~/.config/mpd/pid ] && mpd
 xsetroot -cursor_name left_ptr &
 clipster -d &
 picom --dbus --experimental-backends --backend glx --config ~/.config/picom/picom.conf &
@@ -10,3 +9,5 @@ feh --bg-fill ~/Pictures/wall.jpg
 sxhkd &
 while pgrep -u $UID -x redshift >/dev/null; do sleep 1; done
 redshift &
+yams
+mpd_discord_richpresence -p=6601 --no-idle --fork
--- .zprofile
+++ .zprofile
@@ -1,1 +1,4 @@
 [[ -z $DISPLAY && $XDG_VTNR -eq 1 ]] && exec startx
+
+export PATH=$PATH:/usr/local/bin
+export PATH=$PATH:/home/c/.local/bin
