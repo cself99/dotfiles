5535c3188a97fcbed2e8d80bdf77ab749c1f7ba4
cself99 <cself99@gmail.com>
2020-04-08 12:38:30 -0400

    fix startup order

--- .startup.sh
+++ .startup.sh
@@ -7,7 +7,7 @@ clipster -d &
 picom --dbus --experimental-backends --backend glx --config ~/.config/picom/picom.conf &
 feh --bg-fill ~/Pictures/wall.jpg
 sxhkd &
-while pgrep -u $UID -x redshift >/dev/null; do sleep 1; done
-redshift &
 yams
 mpd_discord_richpresence -p=6601 --no-idle --fork
+while pgrep -u $UID -x redshift >/dev/null; do sleep 1; done
+redshift &
